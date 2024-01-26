de2ec98ed08ccd597e245a2f9d6fb59536f04f0b
Mohamed Hema <mohamedhema2017@gmail.com>
2024-01-26 14:19:07 +0200

    lesson10

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,1 +1,16 @@
-const hello = "Hello World!";
+const fs = require("fs");
+// blocking
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+
+// const textOut = `This is what we know about the avocado ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written!");
+
+// Non Blocking ASYNC
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+  });
+});
+console.log("Will read file!");
